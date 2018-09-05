09f65af5e5dc2d8c3a87487b4986a790d56fd07c
Héctor Aguado <haguadop@gmail.com>
2018-09-05 19:46:53 +0200

    usar styled components con Header/index.js

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background: #333;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px; /* vertical horizontal */
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  display: inline-block;
+  width: 60px;
+  height: 60px;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 30px;
+`;
 
 const Header = ({ logo }) => (
-  <div>
-    <img src={logo} alt="" />
-    <h1>
+  <StyledHeader>
+    <StyledImg src={logo} alt="" />
+    <StyledH1>
 Ejemplo de contador Keepcoding
-    </h1>
-  </div>
+    </StyledH1>
+  </StyledHeader>
 );
+
+Header.propTypes = {
+  logo: PropTypes.string.isRequired,
+};
 
 export default Header;
