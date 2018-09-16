1fea0eebd74036dab6762be251a71e03267faf39
Héctor Aguado <haguadop@gmail.com>
2018-09-16 19:54:09 +0200

    Formulario

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,36 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Template from '../../layout';
 import CounterValue from '../CounterValue';
 import OperationButtons from '../OperationButtons';
 import ResetButtons from '../ResetButtons';
+import ChangeValueForm from '../ChangeValueForm';
 
 const INITIAL_STATE = {
   value: 0,
+  inputValue: '',
 };
 class App extends Component {
   state = INITIAL_STATE;
-
-  componentDidMount = () => {
-    const { maxValue } = this.props;
-    this.interval = setInterval(() => {
-      this.setState(prevState => {
-        if (prevState.value * -1 > maxValue * -1 && prevState.value < maxValue) {
-          return {
-            value: prevState.value + 1,
-          };
-        }
-        return {};
-      });
-    }, 1000);
-  };
-
-  componentWillUnmount = () => {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  };
 
   shouldComponentUpdate = (nextProps, nextState) => {
     if (nextProps.maxValue !== this.props.maxValue) {
@@ -39,14 +21,33 @@ class App extends Component {
     if (nextState.value !== this.state.value) {
       return true;
     }
+    if (nextState.inputValue !== this.state.inputValue) {
+      return true;
+    }
     return false;
   };
 
   updateCounter = event => {
-    console.log(event.target); // <button class="sc-dnqmqq fINBpT" type="button" value="1"> -> saca la etiqueta el botón
+    // console.log(event.target); // <button class="sc-dnqmqq fINBpT" type="button" value="1"> -> saca la etiqueta el botón
     const { value } = event.target; // por destructuring, sacamos value del target
     this.setState(prevState => ({
       value: prevState.value + +value, // + delante de variableString, la convierte en entero. equivale a Number(value) o parseInt(value, 10)
+      inputValue: (prevState.value + +value).toString(),
+    }));
+  };
+
+  updateInputValue = event => {
+    const { value } = event.target;
+    console.log(value);
+    this.setState({
+      inputValue: value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      value: +prevState.inputValue,
     }));
   };
 
@@ -55,13 +56,24 @@ class App extends Component {
   };
 
   render() {
-    const { value } = this.state;
+    const { value, inputValue } = this.state;
     const { maxValue } = this.props;
     return (
       <Template logo="https://keepcoding.io/es/wp-content/uploads/sites/4/2015/05/logo-keepcoding-web.png">
         <CounterValue value={value} />
         {value > maxValue * -1 && value < maxValue ? (
-          <OperationButtons updateCounter={this.updateCounter} />
+          <Fragment>
+            <OperationButtons
+              updateCounter={this.updateCounter}
+              value={value}
+              maxValue={maxValue}
+            />
+            <ChangeValueForm
+              inputValue={inputValue}
+              updateInputValue={this.updateInputValue}
+              onSubmit={this.onSubmit}
+            />
+          </Fragment>
         ) : (
           <ResetButtons resetCounter={this.resetCounter} />
         )}
@@ -79,3 +91,32 @@ App.defaultProps = {
 };
 
 export default App;
+
+// METODO PARA ACTUALIZAR INPUT, pero al final se arrepintión y lo hicimos diréctamente con el estado
+// static getDerivedStateFromProps = (nextProps, prevState) => {
+//   return {
+//     ...prevState,
+//     inputValue: prevState.value,
+//   };
+// };
+
+// METODOS PARA CONTADOR ACTUALICE SOLO
+// componentDidMount = () => {
+//   const { maxValue } = this.props;
+//   this.interval = setInterval(() => {
+//     this.setState(prevState => {
+//       if (prevState.value * -1 > maxValue * -1 && prevState.value < maxValue) {
+//         return {
+//           value: prevState.value + 1,
+//         };
+//       }
+//       return {};
+//     });
+//   }, 1000);
+// };
+
+// componentWillUnmount = () => {
+//   if (this.interval) {
+//     clearInterval(this.interval);
+//   }
+// };
